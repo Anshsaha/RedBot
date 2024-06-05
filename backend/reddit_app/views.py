9a9ea6983c20bdd8f0225c6730a2686be8fecb01
anshaidetic <ansh.saha@idetic.in>
2024-06-05 13:37:19 +0530

    frontend setup and backend basic apis

--- backend/reddit_app/views.py
+++ backend/reddit_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+import requests
+from .reddit_auth import authorize_reddit
+from django.http import JsonResponse
 
-# Create your views here.
+
+class RedditAPIView(APIView):
+
+    def get(self, request):
+        headers = authorize_reddit()
+        res_hot = requests.get(
+            "https://oauth.reddit.com/r/india/hot",
+            headers=headers,
+            params={"limit": "50"},
+        )
+        data = res_hot.json()
+        return JsonResponse({"data": data["data"]["children"]})
